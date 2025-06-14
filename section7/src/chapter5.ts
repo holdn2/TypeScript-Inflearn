4e393b150bea5df7b88f0640b3bd0b10dc915b89
holdn2 <yoocy01@gmail.com>
2025-06-14 11:01:27 +0900

    fix : 프로미스와 제네릭 부분 정리해보면서 틀린 부분 수정

--- section7/src/chapter5.ts
+++ section7/src/chapter5.ts
@@ -4,19 +4,22 @@
 // 비동기 처리의 결과값의 타입을 정해줄 수는 있지만 실패했을 때의 타입을 정해줄 수는 없다.
 const promise = new Promise<number>((resolve, reject) => {
   setTimeout(() => {
-    // resolve(20);
-    reject("~~ 때문에 실패");
+    resolve(20);
+    // reject("~~ 때문에 실패");
   }, 3000);
 });
 
-promise.then((response) => {
-  console.log(response * 10);
-});
-promise.catch((err) => {
-  if (typeof err === "string") {
-    console.log(err);
-  }
-});
+promise
+  .then((response) => {
+    console.log(response * 10);
+  })
+  .catch((err) => {
+    if (typeof err === "string") {
+      console.log(err);
+    } else if (typeof err === "number") {
+      console.log("에러 코드 : ", err);
+    }
+  });
 
 /**
  * 프로미스를 반환하는 함수의 타입을 정의
@@ -26,25 +29,9 @@ interface Post {
   title: string;
   content: string;
 }
-// function fetchPost() {
-//   return new Promise<Post>(() => {
-//     setTimeout((resolve: any, reject: any) => {
-//       resolve({
-//         id: 1,
-//         title: "게시글 제목",
-//         content: "게시글 컨텐츠",
-//       });
-//     }, 3000);
-//   });
-// }
-// const postRequest = fetchPost();
-// postRequest.then((post) => {
-//   post.id;
-// });
-
-function fetchPost(): Promise<Post> {
-  return new Promise(() => {
-    setTimeout((resolve: any, reject: any) => {
+function fetchPost() {
+  return new Promise<Post>((resolve: any, reject: any) => {
+    setTimeout(() => {
       resolve({
         id: 1,
         title: "게시글 제목",
@@ -55,5 +42,22 @@ function fetchPost(): Promise<Post> {
 }
 const postRequest = fetchPost();
 postRequest.then((post) => {
+  console.log(post.id);
   post.id;
 });
+
+// function fetchPost(): Promise<Post> {
+//   return new Promise((resolve: any, reject: any) => {
+//     setTimeout(() => {
+//       resolve({
+//         id: 1,
+//         title: "게시글 제목",
+//         content: "게시글 컨텐츠",
+//       });
+//     }, 3000);
+//   });
+// }
+// const postRequest = fetchPost();
+// postRequest.then((post) => {
+//   console.log(post.id);
+// });
